5fe9b289b96b34e338c03e747023565189e872c7
AntunesGean <gean.antunes@gmail.com>
2022-12-04 10:48:41 -0300

    primeira aula sobre funções

---
+++ funcao/funcao_01.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+void main() {
+  // int a = 2;
+  // int b = 3;
+  // print(a + b);
+  sum(2, 3);
+
+  int c = 4;
+  int d = 5;
+  // print(c + d);
+  sum(c, d);
+
+  sumRandon();
+}
+
+void sum(int a, int b) {
+  print(a + b);
+}
+
+void sumRandon() {
+  int a = Random().nextInt(11);
+  int b = Random().nextInt(11);
+  print('Os valores sorteados foram $a e $b.');
+  print('A soma é ${a + b}.');
+}
