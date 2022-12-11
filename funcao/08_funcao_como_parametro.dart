355dbd1f5ab5f3d15fe3217878a9d20fed42b024
AntunesGean <gean.antunes@gmail.com>
2022-12-11 13:29:01 -0300

    função como parametro - outro exemplo

---
+++ funcao/08_funcao_como_parametro.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+void executarPor(int qtde, Function(String) fn, String valor) {
+  for (int i = 0; i < qtde; i++) {
+    fn(valor);
+  }
+}
+
+void imprimirData(String valor) {
+  var data = DateTime.now();
+  print('Imprimindo data $data');
+}
+
+void main() {
+  var minhaFnPar = (String valor) => print('Valor par: $valor');
+  var minhaFnImpar = (String valor) => print('Valor ímpar: $valor');
+
+  executarPor(2, minhaFnPar, 'Par');
+  executarPor(3, minhaFnImpar, 'Ímpar');
+  executarPor(2, imprimirData, 'Data'); 
+}
