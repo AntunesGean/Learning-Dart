621e07b3bfe1250e2ca94459afac989c4b7c0737
AntunesGean <gean.antunes@gmail.com>
2022-12-11 16:26:19 -0300

    retornar função

---
+++ funcao/09_retornar_funcao.dart
@@ -0,0 +1,16 @@
+int Function(int) sumParcial(int a) {
+  int c = 0;
+
+  return (int b) {
+    return a + b + c;
+  };
+}
+
+void main() {
+  print(sumParcial(2)(10));
+
+  var somaCom10 = sumParcial(10);
+  print(somaCom10(3));
+  print(somaCom10(7));
+  print(somaCom10(19));
+}
