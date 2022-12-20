46f88be6f764f261cfc0a4250826a57ba9e372f7
AntunesGean <gean.antunes@gmail.com>
2022-12-20 20:34:10 -0300

    o básico sobre Class

---
+++ 04_POO/01_class_data.dart
@@ -0,0 +1,20 @@
+class Data {
+  int? dia;
+  int? mes;
+  int? ano;
+}
+
+void main() {
+  var dataAniversario = new Data();
+  dataAniversario.dia = 03;
+  dataAniversario.mes = 10;
+  dataAniversario.ano = 2022;
+
+  Data dataCompra = new Data();
+  dataCompra.dia = 07;
+  dataCompra.mes = 04;
+  dataCompra.ano = 2021;
+
+  print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+}
